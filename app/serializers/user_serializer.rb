a7849d90633af729f1b0a55846d4e1632af9ae77
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2021-09-25 02:26:12 +0100

    add update and show actions, and add test

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,17 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :email
+  attributes :email, :archived, :deleted
+
+  attribute :user_updates do |user|
+    activities = []
+    user.versions.each do |v|
+      changes = PaperTrail.serializer.load(v.object)
+      activities << {
+        modifier: User.find(v.whodunnit).email,
+        action: v.event,
+        changes: changes.slice('id', 'email', 'archived', 'deleted')
+      }
+    end
+    activities
+  end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -8,12 +8,82 @@ RSpec.describe 'Users', type: :request do
       password_confirmation: 'supersecurepassword',
     )
   end
+  let(:auth_token) { authenticate_user(user) }
+  let(:headers) { { 'Authentication' => "Bearer #{auth_token}" } }
+  let!(:users) { create_list(:user, 5) }
+
+  describe 'when user is not authenticated' do
+    it 'returns error message' do
+      get user_path(user), headers: {}
+
+      expect(json['message']).to eq 'Please log in'
+    end
+  end
 
   describe 'GET /index' do
     it 'returns http success' do
-      auth_token = authenticate_user(user)
-      get users_path, headers: { 'Authentication' => "Bearer #{auth_token}" }
+      get users_path, headers: headers
+
       expect(response).to have_http_status(:success)
+    end
+
+    context 'when no filters applied' do
+      it 'returns the users' do
+        get users_path, headers: headers
+
+        expect(json['data'].length).to eq 6 # that is plus the authenticted user
+      end
+    end
+
+    context 'when filters are applied' do
+      it 'returns the users' do
+        get users_path(archived: true), headers: headers
+
+        expect(json['data']).to eq []
+      end
+    end
+  end
+
+  describe 'GET /users/:id' do
+    context 'when user is authenticated and invalid user id is provided' do
+      it 'returns a not found error' do
+        get user_path('ttt'), headers: headers
+
+        expect(response).to have_http_status(:not_found)
+        expect(json['message']).to eq "Couldn't find User with 'id'=ttt"
+      end
+    end
+
+    context 'when user is authenticated and provides a user id' do
+      it 'returns the user' do
+        get user_path(user), headers: headers
+
+        expect(response).to have_http_status(:success)
+        expect(json['data']['id']).to eq user.id.to_s
+      end
+    end
+  end
+
+  describe 'PUT /users/:id' do
+    context 'when a user tries to archive/unarchive/delete his/her account' do
+      it 'returns error message' do
+        put user_path(user), headers: headers
+
+        expect(response).to have_http_status(:forbidden)
+        expect(json['message']).to eq 'You cannot archive/unarchive/delete your account'
+      end
+    end
+
+    context 'when a user tries to archive another users account' do
+      it 'should archive the user' do
+        put user_path(users.first),
+            headers: headers,
+            params: { user: { archived: true } }
+
+        expect(response).to have_http_status(:success)
+        expect(json['data']['attributes']['archived']).to eq true
+        expect(json['data']['attributes']['user_updates']).not_to be_empty
+      end
     end
   end
 end
